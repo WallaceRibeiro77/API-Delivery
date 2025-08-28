c2e3a68010a91673c4b5be3002718ddae9f84fc5
Wallace Ribeiro <wallaceribeiro72@gmail.com>
2025-08-28 10:24:39 -0300

    implementando a validação com zod e encriptação de senha com bcrypt

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from "express"
+import { hash } from "bcrypt"
+import { z } from "zod"
 
 class UsersController{
-    create(request: Request, response: Response){
+    async create(request: Request, response: Response){
+        const bodySchema = z.object({
+            name: z.string().trim().min(3),
+            email: z.string().email(),
+            password: z.string().min(6),
+        })
+
+        const { name, email, password } = bodySchema.parse(request.body)
+
+        const hashedPassword = await hash(password, 8)
+
         return response.json({ message: "created" })
     }
 }
